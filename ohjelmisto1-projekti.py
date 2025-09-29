ae11773b9637274a4fc641ea00c2347ca3813a50
lucashav-dot <lucashav@metropolia.fi>
2025-09-29 11:26:56 +0300

    Update ohjelmisto1-projekti.py
    noppa

--- ohjelmisto1-projekti.py
+++ ohjelmisto1-projekti.py
@@ -1,16 +1,17 @@
 import random
 import tarinaesim
-from geopy import distance
 import boss
-
+from geopy import distance
 import mysql.connector
+
+from boss import golden_ball
 
 conn = mysql.connector.connect(
     host='localhost',
     port=3306,
-    database='demopelikanta',
-    user='demo',
-    password='peli',
+    database='',
+    user='',
+    password='',
     autocommit=True
 )
 
@@ -26,7 +27,7 @@ FROM airport
 WHERE iso_country = 'BR' 
 AND type='medium_airport'
 ORDER by RAND()
-LIMIT 31;"""
+LIMIT 30;"""
     cursor = conn.cursor(dictionary=True)
     cursor.execute(sql)
     result = cursor.fetchall()
@@ -144,6 +145,8 @@ money = 1000
 # start range in km = 2000
 player_range = 2000
 
+golden_ball = 0
+
 # score = 0
 score = 0
 
@@ -165,12 +168,13 @@ current_airport = start_airport
 game_id = create_game(money, player_range, start_airport, player, all_airports)
 
 # GAME LOOP
+
 while not game_over:
     # get current airport info
     airport = get_airport_info(current_airport)
     # show game status
     print(f'''You are at {airport['name']}.''')
-    print(f'''You have {money:.0f}$ and {player_range:.0f}km of range.''')
+    print(f"\033[93mYou have {golden_ball} golden balls\033[0m")
     # pause
     input('\033[32mPress Enter to continue...\033[0m')
     # if airport has goal ask if player wants to open it
@@ -178,74 +182,65 @@ while not game_over:
     goal = check_goal(game_id, current_airport)
     snail += 1 # add + 1 on each loop
     if snail == 5: # snail "catches" you after reaching value "5"
-        print('Game over!')
+        print('Snail Got you, Game over!')
         game_over = True
 
-
     if goal:
-        snail -= 1 # subtract 1 when finding treasure, snail "slows down" for one turn
+        snail -= 1  # subtract 1 when finding treasure, snail "slows down" for one turn
         question = input(
-            f'''Do you want to open lootbox for {"100$ or " if money > 100 else ""}{"50km range" if player_range > 50 else ""}? M = money, R = range, enter to skip: ''')
-        if not question == '':
-            if question == 'M':
-                money -= 100
-            elif question == 'R':
-                player_range -= 50
+            f'''Do you want to open lootbox (Y/N): ''')
+        if not question == 'N':
+
             if goal['money'] > 0:
                 money += goal['money']
                 print(f'''Congratulations! You found {goal['name']}. That is worth {goal['money']}$.''')
                 print(f'''You have now {money:.0f}$''')
             elif goal['money'] == 0:
-                win = True
-                print(f'''Congratulations! You found the diamond. Now go to start.''')
+                golden_ball += 1
+                print(f'''Congratulations! You found A Golden Ball.''')
                 input("\033[32mPress Enter to continue...\033[0m")
             else:
                 money = 0
                 print(f'''Oh no! You have been robbed. You lost all your money''')
                 input("\033[32mPress Enter to continue...\033[0m")
 
-    # ask to buy fuel/range
-    if money > 0:
-        question2 = input('Do you want to by fuel? 1$ = 2km of range. Enter amount or press enter. ')
-        if not question2 == '':
-            question2 = float(question2)
-            if question2 > money:
-                print(f'''You don't have enough money.''')
-            else:
-                player_range += question2 * 2
-                money -= question2
-                print(f'''You have now {money:.0f}$ and {player_range:.0f}km of range''')
-        # pause
-        input("\033[32mPress Enter to continue...\033[0m")
 
-    # if no range, game over
+    # ask to buy fuel/range
+
     # show airports in range. if none, game over
-    airports = airports_in_range(current_airport, all_airports, player_range)
-    print(f'''\033[34mThere are {len(airports)} airports in range: \033[0m''')
+    airports = all_airports
+
     if len(airports) == 0:
         print('You are out of range.')
         game_over = True
     else:
-        print(f'''Airports: ''')
-        for airport in airports:
-            ap_distance = calculate_distance(current_airport, airport['ident'])
-            print(f'''{airport['name']}, icao: {airport['ident']}, distance: {ap_distance:.0f}km''')
+
+        r = random.randint(1, 6)
+        print(f"\033[32mThe golden dice gave you {r} new airports.\033[0m")
+        random_list = random.sample(airports, r)
+        for n, item in enumerate(random_list):
+            ap_distance = calculate_distance(current_airport, item['ident'])
+            print(f"{n + 1}. {item['name']}, {item['ident']}")
+
         # ask for destination
-        dest = input('Enter destination icao: ')
-        selected_distance = calculate_distance(current_airport, dest)
-        player_range -= selected_distance
-        update_location(dest, player_range, money, game_id)
-        current_airport = dest
+        ask = int(input("\033[32mSelect one of the following airports:\033[0m"))
+        dest = random_list[ask - 1]  # no nested indexing
+        icao=dest['ident']
+        selected_distance = calculate_distance(current_airport, icao)
+
+
+        update_location(icao, player_range, money, game_id)
+        current_airport = icao
         if player_range < 0:
             game_over = True
-    # if diamond is found and player is at start, game is won
-    if win and current_airport == start_airport:
-        print(f'''You won! You have {money}$ and {player_range}km of range left.''')
+
+
+    # if 5 Golden balls, game is won
+    if golden_ball == 100:
         game_over = True
 
 
 # if game is over loop stops
 # show game result
-print(f'''{'You won!' if win else 'You lost!'}''')
-print(f'''You have {money:.0f}$''')
-print(f'''Your range is {player_range:.0f}km''')
+print(f'''{'You won!' if golden_ball == 1 else 'You lost!'}''')
+print(f"You Found {golden_ball} Golden Balls")
