de8005a3cd931dfb60cdebaa62cc4a4b78d6455f
Kareemdub <kareem.werlander@metropolia.fi>
2025-09-30 12:01:53 +0300

    Update ohjelmisto1-projekti.py
    add boss to program

--- ohjelmisto1-projekti.py
+++ ohjelmisto1-projekti.py
@@ -1,17 +1,15 @@
 import random
 import tarinaesim
-import boss
 from geopy import distance
 import mysql.connector
 
-from boss import golden_ball
 
 conn = mysql.connector.connect(
     host='localhost',
     port=3306,
-    database='',
-    user='',
-    password='',
+    database='demogame',
+    user='root',
+    password='admin1234',
     autocommit=True
 )
 
@@ -42,6 +40,125 @@ def get_goals():
     result = cursor.fetchall()
     return result
 
+def boss():
+    global snail
+    boss_chance = random.randint(1, 2)
+    smash = random.randint(1, 2)
+    stab = random.randint(-1, 3)  # weapon stats per airport boss
+    shoot = random.randint(-2, 4) #real value
+    #shoot = 7 #for fast testing
+    boss_hp = 8
+    player_hp = 8  # defining hp values
+    game = True
+    if boss_chance == 1: #if fight
+        print("The ball is guarded by a golden guardian. The only way forward is to fight the guardian!")
+        print("You fight the guardian")
+        while game == True: #while loop to play until hp reaches 0
+
+            input("Press enter to initiate the next round!")
+            print("------------------------------")
+            guardian_attack = random.randint(1, 2)
+            print(f"The guardian attacks you dealing {guardian_attack} damage")
+            player_hp = player_hp - guardian_attack
+            if player_hp <= 0:  # when lost
+                golden_ball = 0
+                snail = snail + 1
+                print("You have lost the battle")
+                print("The Snail gains some speed")
+                print(snail)
+                try_again = input("Do you want to try again? Y/N:").capitalize().strip()
+                if try_again == "Y" and snail < 5:
+                    print(f"The snail is {5 - snail} turns away from you")
+                    print("You prepare to fight again.")
+                    player_hp = 8
+                    boss_hp = 8
+                elif try_again == "N":
+                    print("You flee from the guardian abandoning the golden ball.")
+                    game == False
+                    return golden_ball, snail
+                if snail == 5:
+                    return golden_ball, snail
+            elif game == True:
+                print(f"You have {player_hp} health left")
+                attack = input("Would you like to smash, stab or shoot the guardian?").lower()
+                if attack == "stab":
+                    if stab > 0:  # stab outcomes
+                        boss_hp = boss_hp - stab
+                        print(f"You stab the guardian with your sword dealing {stab} amount of damage")
+                        print(f"The guardian has {boss_hp} health left")
+                        print(f"You have {player_hp} health left")
+                    elif stab == 0:
+                        print("Your stab is ineffective.")
+                        print(f"The guardian has {boss_hp} health left")
+                        print(f"You have {player_hp} health left")
+                    elif stab < 0:
+                        print(f"You try to stab the guardian, but you slip dealing {stab} damage to "
+                              f"yourself instead")
+                        player_hp = player_hp + stab #player taking negative damage
+                        print(f"The guardian has {boss_hp} health left")
+                        print(f"You have {player_hp} health left")
+                if attack == "shoot":  # shoot outcomes
+                    if shoot > 0:
+                        boss_hp = boss_hp - shoot
+                        print(f"You shoot the guardian and deal {shoot} amount of damage")
+                        print(f"The guardian has {boss_hp} health left")
+                        print(f"You have {player_hp} health left")
+                    elif shoot == 0:
+                        print("Your weapon seems to be ineffective.")
+                        print(f"The guardian has {boss_hp} health left")
+                        print(f"You have {player_hp} health left")
+                    elif shoot < 0:
+                        print(f"You try to shoot the guardian, but your gun explodes dealing {shoot} damage to "
+                              f"yourself instead")
+                        player_hp = player_hp + shoot #player taking negative damage
+                        print(f"The guardian has {boss_hp} health left")
+                        print(f"You have {player_hp} health left")
+                if attack == "smash":  # smash outcomes
+                    if stab < 2 and shoot < 2: # to prevent a situation where no possilble weapon can win
+                        smash = 2
+                        boss_hp = boss_hp - smash
+                        print(smash)
+                        print(f"You smash with your hammer the boss and deal {smash} amount of damage")
+                        print(f"The guardian has {boss_hp} health left")
+                        print(f"You have {player_hp} health left")
+                    elif smash > 0:
+                        boss_hp = boss_hp - smash
+                        print(f"You smash with your hammer the boss and deal {smash} amount of damage")
+                        print(f"The guardian has {boss_hp} health left")
+                        print(f"You have {player_hp} health left")
+
+                if boss_hp <= 0: #when victorious
+                    golden_ball =+ 1
+                    snail = snail - 1
+                    print(f"---You have defeated the guardian!!!---")
+                    print(f"You feel the snail slowing down")
+                    print("You grab the golden ball!")
+                    game == False
+                    return golden_ball, snail
+                elif player_hp <= 0: #when lost
+                    golden_ball = 0
+                    snail = snail + 1
+                    print("You have lost the battle")
+                    print("The Snail gains some speed")
+                    try_again = input("Do you want to try again? Y/N:").upper().strip()
+                    if try_again == "Y" and snail < 5:
+                            print(f"The snail is {5- snail} turns away from you")
+                            print("You prepare to fight again.")
+                            player_hp = 8
+                            boss_hp = 8
+                    elif try_again == "N":
+                            print("You flee from the guardian abandoning the golden ball.")
+                            game == False
+                            return golden_ball, snail
+                    if snail == 5:
+                        return golden_ball, snail
+
+    elif boss_chance == 2:# return ball if no fight
+        golden_ball =+ 1
+        snail = snail - 1
+        print("You grab the golden ball!")
+        print("You feel the snail slowing down")
+        return golden_ball, snail
 
 # create new game
 def create_game(start_money, p_range, cur_airport, p_name, a_ports):
@@ -177,44 +294,46 @@ while not game_over:
     print(f"\033[93mYou have {golden_ball} golden balls\033[0m")
     # pause
     input('\033[32mPress Enter to continue...\033[0m')
-    # if airport has goal ask if player wants to open it
-    # check goal type and add/subtract money accordingly
+    # check goal type
     goal = check_goal(game_id, current_airport)
     snail += 1 # add + 1 on each loop
-    if snail == 5: # snail "catches" you after reaching value "5"
-        print('Snail Got you, Game over!')
+    print(snail)
+    if snail > 9: # snail "catches" you after reaching value "5"
+        print('Snail Got you, Game over!'a)
         game_over = True
+        break
 
-        if goal: 
-        f"There's something in the airport..."
-            if goal['money'] == 1: 
-                money += goal['money']
-                snail -= 2
-                print(f"Congratulations! You've found a snail shield")
-                print(f"The snail won't be able to touch you for a while")
-                input("\033[32mPress Enter to continue...\033[0m")
-            elif goal['money'] == 2:
-                money += goal['money']
-                snail -= 1
-                print(f"Congratulations! You've found snail adhesive")
-                print(f"The Snail slows down...")
-                input("\033[32mPress Enter to continue...\033[0m")
-            elif goal['money'] == 3: 
-                money += goal['money']
-                snail -= 1
-                golden_ball += 1
-                print(f"Congratulations! You've found a golden ball.")
-                input("\033[32mPress Enter to continue...\033[0m")
-            elif goal['money'] == 4: 
-                snail += 1
-                print(f'''You get a horrible headache.''')
-                print("The snail catches up...")
-                input("\033[32mPress Enter to continue...\033[0m")
-            else:   
-                snail += 2
-                print(f'''Your stomach aches like crazy...''')
-                print("The snail lurks in closer...")
-                input("\033[32mPress Enter to continue...\033[0m")
+    if goal:
+        print(f"There's something in the airport...")
+        if goal['money'] == 1:
+            money += goal['money']
+            snail -= 2
+            print(f"Congratulations! You've found a snail shield")
+            print(f"The snail won't be able to touch you for a while")
+            input("\033[32mPress Enter to continue...\033[0m")
+        elif goal['money'] == 2:
+            money += goal['money']
+            snail -= 1
+            print(f"Congratulations! You've found snail adhesive")
+            print(f"The Snail slows down...")
+            input("\033[32mPress Enter to continue...\033[0m")
+        elif goal['money'] == 3:
+            new_golden_ball, snail = boss()
+            golden_ball = new_golden_ball + golden_ball  # to get new value for golden ball
+            # snail = snail + new_snail #to get new value for snail
+            print(f"You have a total of {golden_ball} golden balls")
+            print(f"Snail is {5 - snail} turns away from you")  # updated information from after the boss"""
+            input("\033[32mPress Enter to continue...\033[0m")
+        elif goal['money'] == 4:
+            snail += 1
+            print(f'''You get a horrible headache.''')
+            print("The snail catches up...")
+            input("\033[32mPress Enter to continue...\033[0m")
+        else:
+            snail += 2
+            print(f'''Your stomach aches like crazy...''')
+            print("The snail lurks in closer...")
+            input("\033[32mPress Enter to continue...\033[0m")
 
 
     # ask to buy fuel/range
