f16d27385db0eddc12109409d0ca2576fe8df929
kaspehu <kaspehu@metropolia.fi>
2025-10-01 10:36:36 +0300

    Update tarinaesim.py
    lisätty lyhyt epilogi ja jätetty tekstinmuokkausehdot pois toistaiseksi

--- tarinaesim.py
+++ tarinaesim.py
@@ -1,5 +1,5 @@
-import textwrap
-tarina = """You wake up with a hazy memory and a piercing headache. You do not know where you are nor why.
+#import textwrap
+tarina = """\033[32mYou wake up with a hazy memory and a piercing headache. You do not know where you are nor why.
 The last thing in your mind is a faint recall of a decision you've made. You're not yet aware of this,
 but your greed related to this decision has caused you your current situation.
 
@@ -10,13 +10,18 @@ you will have to find golden balls scattered across airports in Brasil and make 
 
 This golden dice will be of use on your journey, good luck."  Flabbergasted by your current situation
 you take one look at the golden dice, and the woman is gone. Something tells you there is truth in her
-words...so, will you throw the dice? Or face the Snail?"""
+words...so, will you throw the dice? Or face the Snail?\033[0m"""
+
+epilogi = """\033[34mYou've cast away the curse, you've been freed from the snail. You learned to give up your greed
+and are ready for a new step in life. You learn to look at the world in a different way. It feels like your 
+senses have been sharpened and you can appreciate even the smallest things in life.\033[0m
+"""
 
 # Set column width to 80 characters
-wrapper = textwrap.TextWrapper(width=80, break_long_words=False, replace_whitespace=False)
+#wrapper = textwrap.TextWrapper(width=80, break_long_words=False, replace_whitespace=False)
 # Wrap text
-word_list = wrapper.wrap(text=tarina)
+#word_list = wrapper.wrap(text=tarina)
 
 
-def haetarina():
-    return word_list
+#def haetarina():
+    #return word_list
